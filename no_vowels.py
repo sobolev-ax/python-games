613038abf65c0e998316ff0c918ce4161934f1f1
Alexander Sobolev <sobolev.ax@gmail.com>
2019-09-14 19:31:14 +0300

    No Vowels

---
+++ no_vowels.py
@@ -0,0 +1,15 @@
+# Только согласные
+# Демонострирует, как создавать новые строки из исхожных с помощью цикла for
+
+message = input("Введите текст: ")
+new_message = ""
+
+VOWELS = "aeiouаеёиоуыэюя"
+
+for letter in message:
+	if letter.lower() not in VOWELS:
+		new_message += letter
+		print("Создана новая строка:", new_message)
+
+print("\nВот ваш текст с изъятыми гласными буквами:", new_message)
+input("\n\nНажмите Enter, чтобы выйти.")
